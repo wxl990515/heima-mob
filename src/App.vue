8a9b4ae14eddfd0e84425657f38e1e6b61db5b11
wxl <wxl990515@163.com>
2022-01-17 00:31:13 +0800

    字体图标和vant组件库的引入初始化

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
-    <router-view/>
+    <i class="iconfont iconlishi"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-search v-model="value" placeholder="请输入搜索关键词" />
+    <router-view />
   </div>
 </template>
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      value: ''
+    }
+  }
 }
 </script>
 <style lang="less">
-
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
 import './styles/index.less'
+// 加载vant核心组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 注册vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
